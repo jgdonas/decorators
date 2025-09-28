f2ae822ef06c3ca9fed0cff1abd36d5aca38472d
Jose Antonio González Doñas <jose.gonzalez.donas@gmail.com>
2025-09-28 18:47:25 +0200

    Ci/publish npm (#5)
    * feat: show tests results as junit report
    
    * chore: bump version on releases branch merges only
    
    * fix: use GH_PAT for version bump push
    
    * feat: enhance CI workflow with input ref and improved description
    
    * fix: correct indentation in CI workflow inputs section
    
    * fix: update test reporter to jest-junit in CI workflow
    
    * feat: update test reporter to jest-junit and add junit.xml for test results
    
    * fix: update test reporter to jest-junit in CI workflow
    
    * chore: refine vitest config
    
    * chore: add extension to recommendations
    
    * fix: update bump version workflow to trigger on pull request closure
    
    * chore: add copilot sugestions
    
    * fix: ensure bump version job only runs on merged pull requests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,11 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    reporters: ['default', 'junit'],
     coverage: {
       reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.ts'],
+      exclude: ['src/index.ts', 'src/interfaces.ts'],
     },
     include: ['**/*.test.ts'],
   },
